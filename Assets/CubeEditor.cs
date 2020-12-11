33116d70c9b66c6606648b7bf991cc088bd71ade
squidinkettle <jchabol309@hotmail.com>
2020-12-11 10:36:52 +0100

    Cubes can used as grids

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+public class CubeEditor : MonoBehaviour
 {
 
+    TextMesh textmesh;
     [Range(1f, 20f)] [SerializeField] float gridSize;
     // Update is called once per frame
+    void Start()
+    {
+
+    }
     void Update()
     {
         Vector3 snapPos;
+        textmesh = GetComponentInChildren<TextMesh>();
+
+
+
         snapPos.x = Mathf.RoundToInt(transform.position.x/gridSize)* gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        textmesh.text = snapPos.x/gridSize +","+ snapPos.z/gridSize;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
     }
